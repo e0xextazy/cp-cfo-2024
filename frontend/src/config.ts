10c74ab59926d8762b55a5eada2f8cbadd5be548
= <=>
2024-04-28 02:57:25 +0800

    feat: add readme

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,4 +1,2 @@
-export const API_URL = import.meta.env.VITE_API_HOST;
+export const API_URL = import.meta.env.VITE_API_HOST || 'http://localhost:8080/';
 export const ENABLE_MOCK = import.meta.env.VITE_ENABLE_MOCK === 'true';
-
-console.log(import.meta.env, ENABLE_MOCK, API_URL);
